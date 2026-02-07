8e1bc7f5d34aca41f86ada681137d60341c2de9c
Hung Nguyen <hung@MacBook-Pro-cua-nguyen.local>
2026-02-08 01:23:15 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.ophim.live',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
+  // Tắt strict mode nếu bạn thấy nó log 2 lần (tùy chọn)
+  reactStrictMode: false,
 };
 
 export default nextConfig;
